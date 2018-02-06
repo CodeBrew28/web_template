a0dc5dacfbdbf9504f92a226b77a9ea77bfa8e49
Timothy Ko <timothy.l.ko@gmail.com>
2018-02-06 09:58:28 -0600

    test

--- src/queries/User.js
+++ src/queries/User.js
@@ -1,12 +1,12 @@
-import { GraphQLString } from 'graphql';
-import User from './../types/UserType.js';
+import { GraphQLString } from 'graphql'
+import User from './../types/UserType.js'
 
 const usertype = {
   type: User,
-  args: { email: { type: GraphQLString }, password: { type: GraphQLString }, },
+  args: { email: { type: GraphQLString }, password: { type: GraphQLString } },
   resolve() {
-    return 'Welcome!';
-  },
-};
-
-export default usertype;
+    return 'Welcome!'
+  }
+}
+;('hello')
+export default usertype
